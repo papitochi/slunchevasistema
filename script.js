29e065db1123c20a90c69bb3e39d9b9b25adcd6c
papitochi <59375398+papitochi@users.noreply.github.com>
2025-12-14 12:19:15 +0200

    new planets

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sun = document.querySelector(".sun");
 const orbits = document.querySelectorAll(".orbit");
 
 sun.addEventListener("click", () => {
-  orbits.forEach((orbit) => {
-    orbit.classList.toggle("paused");
-  });
+	orbits.forEach((orbit) => {
+		orbit.classList.toggle("paused");
+	});
 });
