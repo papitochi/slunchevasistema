71b7778048c09216f925cf09082c57f337fec2a4
papitochi <59375398+papitochi@users.noreply.github.com>
2025-12-30 13:32:58 +0200

    Final Commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const sun = document.querySelector(".sun");
-const orbits = document.querySelectorAll(".orbit");
+const navigation = document.getElementById("navigation");
+const nav = document.querySelector("nav");
+navigation.addEventListener("click", () => {
+	nav.classList.toggle("visible");
+});
 
-sun.addEventListener("click", () => {
-	orbits.forEach((orbit) => {
-		orbit.classList.toggle("paused");
-	});
+// Ако кликането върху страницата, е върху елемент,
+// който не се съдържа в менюто (nav) или в бутона (navigation),
+// когато nav има клас visible, да се скрива dropdown менюто.
+
+document.body.addEventListener("click", (e) => {
+	if (!nav.contains(e.target) && !navigation.contains(e.target) && nav.classList.contains("visible")) {
+		nav.classList.remove("visible");
+	}
 });
